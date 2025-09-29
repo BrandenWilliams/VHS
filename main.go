8d6531e5830312678443a299d26f82282abab1dd
BrandenWilliams <BrandenWilliams07@gmail.com>
2025-09-28 20:01:25 -0700

    basic wails setup

--- main.go
+++ main.go
@@ -1,32 +1,36 @@
 package main
 
 import (
-	"log"
+	"embed"
 
-	"github.com/BrandenWilliams/VHS/ffconvert"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
+//go:embed all:frontend/dist
+var assets embed.FS
+
 func main() {
-	var (
-		ffc ffconvert.FFConvert
+	// Create an instance of the app structure
+	app := NewApp()
 
-		// test vars
-		inDir     string
-		destDir   string
-		crf       int
-		preset    string
-		overwrite bool
-	)
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "VHS",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
 
-	inDir = "./videos/"
-	destDir = "./newvideos"
-	crf = 23
-	preset = ""
-	overwrite = true
-
-	ffc.NewFFConvert(inDir, destDir, crf, preset, overwrite, 1)
-
-	if err := ffc.ConvertVideos(); err != nil {
-		log.Fatal(err)
+	if err != nil {
+		println("Error:", err.Error())
 	}
 }
