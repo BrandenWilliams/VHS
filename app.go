57b200829004d8da3ddbf03023ce2edc300f1d81
BrandenWilliams <BrandenWilliams07@gmail.com>
2025-10-02 12:36:14 -0700

    prep app struct

--- app.go
+++ app.go
@@ -3,25 +3,36 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/BrandenWilliams/VHS/ffconvert"
+	"github.com/BrandenWilliams/VHS/ffconvert/linuxcliargs"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+
+	ffc     ffconvert.FFConvert
+	preSets []linuxcliargs.PreSet
 }
 
-// NewApp creates a new App application struct
 func NewApp() *App {
 	return &App{}
+}
+
+func (a *App) getPreSets() {
+	a.preSets = a.ffc.LCliA.GetPreSets()
 }
 
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+
+	a.getPreSets()
 }
 
-// Greet returns a greeting for the given name
+// DELETE LONG TERM
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
