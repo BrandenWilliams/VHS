223b3c4c8b66e6970ce8674210b60030cda215d0
BrandenWilliams <BrandenWilliams07@gmail.com>
2025-10-17 11:50:55 -0700

    basic flag parse setup

---
+++ flagparse/printfuncs.go
@@ -0,0 +1,25 @@
+package flagparse
+
+import "fmt"
+
+func printPresets() {
+	fmt.Printf("Preset List (use number):\n" +
+		"1) Libx264 (default)\n" +
+		"2) VTB264\n" +
+		"3) SVTAV1\n" +
+		"4) Libx265\n\n")
+}
+
+func printUsage() {
+	fmt.Printf("VHS Version 0.1\n" +
+		"Usage:\n" +
+		"	VHS -in INPUT -out OUTPUT [options]\n\n" +
+		"Options:\n" +
+		"	-ps PRESET\n\n" +
+		"Preset List (use number):\n" +
+		"1) Libx264 (default)\n" +
+		"2) VTB264\n" +
+		"3) SVTAV1\n" +
+		"4) Libx265\n\n" +
+		"Default Flags:\n")
+}
